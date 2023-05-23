2876ccc6c77411385c109709e84a4d08e3e10ab9
JQ <xujacque@gmail.com>
2023-05-23 10:31:42 -0400

    select elements for dom manipulation

---
+++ src/utils/game1.js
@@ -0,0 +1,8 @@
+const countMoves = document.querySelector('.count-moves');
+const timer = document.querySelector('.timer');
+const startBtn = document.querySelector('.start-btn');
+const restartBtn = document.querySelector('.restart-btn');
+
+startBtn.addEventListener('click', () => {})
+restartBtn.addEventListener('click', () => {})
+
---
+++ src/utils/game2.js
@@ -0,0 +1,7 @@
+const playerScore = document.querySelector('.player-score');
+const computerScore = document.querySelector('.computer-score');
+const rockBtn = document.querySelector('.rock-btn');
+const paperBtn = document.querySelector('.paper-btn');
+const scissorsBtn = document.querySelector('.scissors-btn');
+const resetBtn = document.querySelector('.reset-btn');
+const displayResult = document.querySelector('.display-result');
