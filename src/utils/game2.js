16bb6d2fd2f4f1d051fed010b577887e28639fa4
JQ <xujacque@gmail.com>
2023-05-24 19:58:24 -0400

    set up game 2

--- src/utils/game2.js
+++ src/utils/game2.js
@@ -1,7 +1,8 @@
 const playerScore = document.querySelector('.player-score');
 const computerScore = document.querySelector('.computer-score');
-const rockBtn = document.querySelector('.rock-btn');
-const paperBtn = document.querySelector('.paper-btn');
-const scissorsBtn = document.querySelector('.scissors-btn');
 const resetBtn = document.querySelector('.reset-btn');
 const displayResult = document.querySelector('.display-result');
+
+const rockBtn = document.getElementById('rock-btn');
+const paperBtn = document.getElementById('paper-btn');
+const scissorsBtn = document.getElementById('scissors-btn');
