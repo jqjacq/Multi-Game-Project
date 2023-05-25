057cc530788f559c998a317833b30be0e9e9c469
JQ <xujacque@gmail.com>
2023-05-24 20:41:59 -0400

    setting up game6

--- src/utils/game6.js
+++ src/utils/game6.js
@@ -6,3 +6,11 @@ const food = document.getElementById("food");
 const displayResult = document.querySelector('.display-result');
 const startBtn = document.querySelector('.start-btn');
 const restartBtn = document.querySelector('.restart-btn');
+
+const snakeCanvas = document.getElementById("snakeCanvas");
+const snakeCanvas_ctx = gameCanvas.getContext("2d");
+
+gameCanvas_ctx.fillStyle = 'lightblue';  
+gameCanvas_ctx.strokestyle = 'darkblue';
+gameCanvas_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);  
+gameCanvas_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
