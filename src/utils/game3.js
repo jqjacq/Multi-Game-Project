6de6979c783bf787ae61b8ec4a8f5bad8569354c
Jacqueline Xu <85534903+jqjacq@users.noreply.github.com>
2023-06-02 18:39:46 -0400

    starts at button & displays winner

--- src/utils/game3.js
+++ src/utils/game3.js
@@ -1,98 +1,100 @@
 // const playerScore = document.querySelector('.player-score');
 // const computerScore = document.querySelector('.computer-score');
-const startBtn = document.querySelector('.start-btn');
-const displayResult = document.getElementById('message');
-const restartBtn = document.querySelector('.restart-btn');
-const board = document.querySelector('.gameBoard');
-const boxes = document.querySelectorAll('[data-cell]');
+const startBtn = document.querySelector(".start-btn");
+const displayResult = document.getElementById("message");
+const restartBtn = document.querySelector(".restart-btn");
+const board = document.querySelector(".gameBoard");
+const boxes = document.querySelectorAll("[data-cell]");
 const winningCombos = [
-    [0, 1, 2], // horizontal
-    [3, 4, 5], // horizontal
-    [6, 7, 8], // horizontal
-    [0, 3, 6], // vertical
-    [1, 4, 7], // vertical
-    [2, 5, 8], // vertical
-    [0, 4, 8], // diagonal
-    [2, 4, 6] // diagonal
+  [0, 1, 2], // horizontal
+  [3, 4, 5], // horizontal
+  [6, 7, 8], // horizontal
+  [0, 3, 6], // vertical
+  [1, 4, 7], // vertical
+  [2, 5, 8], // vertical
+  [0, 4, 8], // diagonal
+  [2, 4, 6], // diagonal
 ];
 let playerX;
 let playerO;
 let isPlayerOTurn = false;
 let currentTurn;
 
-startGame();
-
 function startGame() {
-    console.log('clicked');
-    isPlayerOTurn = false;
-    boxes.forEach(box => {
-        box.classList.remove('playerX');
-        box.classList.remove('playerO');
-        box.removeEventListener('click', handleBoxClick);
-        box.addEventListener('click', handleBoxClick, { once: true });
-    });
-    setPlayerHover();
-    displayResult.classList.remove('show');
+  isPlayerOTurn = false;
+  boxes.forEach((box) => {
+    box.classList.remove("playerX");
+    box.classList.remove("playerO");
+    box.removeEventListener("click", handleBoxClick);
+    box.addEventListener("click", handleBoxClick, { once: true });
+  });
+  setPlayerHover();
+  displayResult.classList.remove("show");
 }
 
 function handleBoxClick(e) {
-    const box = e.target;
-    let currentTurn = isPlayerOTurn ? 'playerO' : 'playerX';
-    placeMark(box, currentTurn);
-    if (checkWin(currentTurn)) {
-        endGame(false);
-    } else if (isDraw()) {
-        endGame(true);
-    } else {
-        swapTurns();
-        setPlayerHover();
-    }
-    console.log(currentTurn);
+  const box = e.target;
+  let currentTurn = isPlayerOTurn ? "playerO" : "playerX";
+  placeMark(box, currentTurn);
+  if (checkWin(currentTurn)) {
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
+    swapTurns();
+    setPlayerHover();
+  }
 }
 //End game
 function endGame(draw) {
-    if (draw) {
-        displayResult.innerText = 'Draw!';
-    } else {
-        displayResult.innerText = `${isPlayerOTurn ? "O's" : "X's"} Wins!`;
-    }
-    displayResult.classList.add('show');
+  if (draw) {
+    displayResult.innerText = "Draw!";
+  } else {
+    displayResult.innerText = `${isPlayerOTurn ? "O's" : "X's"} Wins!`;
+  }
+  displayResult.classList.add("show");
+  boxes.forEach((box) => {
+    box.removeEventListener("click", handleBoxClick);
+  });
+  console.log(displayResult);
 }
-//Check for draw 
+//Check for draw
 function isDraw() {
-    return [...boxes].every(box => {
-        return box.classList.contains(playerX) || box.classList.contains(playerO);
-    });
+  return [...boxes].every((box) => {
+    return box.classList.contains(playerX) || box.classList.contains(playerO);
+  });
 }
 //Add mark to board
 function placeMark(box, currentTurn) {
-    box.classList.add(currentTurn);
+  box.classList.add(currentTurn);
 }
 // Swap turns
 function swapTurns() {
-    isPlayerOTurn = !isPlayerOTurn;
+  isPlayerOTurn = !isPlayerOTurn;
 }
 //Player's turn to place mark
 function setPlayerHover() {
-    board.classList.remove(playerX);
-    board.classList.remove(playerO);
-    if (isPlayerOTurn) {
-        board.classList.add(playerO);
-    } else {
-        board.classList.add(playerX);
-    }
+  board.classList.remove(playerX);
+  board.classList.remove(playerO);
+  if (isPlayerOTurn) {
+    board.classList.add(playerO);
+  } else {
+    board.classList.add(playerX);
+  }
 }
 //Check for winning condition
-function checkWin(currentClass) {
-    return winningCombos.some(combo => {
-        return combo.every(index => {
-            return boxes[index].classList.contains(currentTurn);
-        });
+function checkWin(currentTurn) {
+  return winningCombos.some((combo) => {
+    return combo.every((index) => {
+      return boxes[index].classList.contains(currentTurn);
     });
+  });
 }
 function restartGame() {
-    startGame();
+  startGame();
+  displayResult.textContent = "";
+  displayResult.classList.remove("show");
 }
 
-startBtn.addEventListener('click', startGame);
-restartBtn.addEventListener('click', restartGame);
+startBtn.addEventListener("click", startGame);
+restartBtn.addEventListener("click", restartGame);
