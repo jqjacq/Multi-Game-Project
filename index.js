a3a553cdd696c3700b28308217c9ebc013fc800d
Jacqueline Xu <85534903+jqjacq@users.noreply.github.com>
2023-06-10 03:44:03 -0400

    debug box clicking issue by change order of anchor link and div game name

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-
-
 //Add gradient to the background of each grid item when hover
-const gridItems = document.querySelectorAll('.grid-item');
-gridItems.forEach(gridItem => {
-  gridItem.addEventListener('mouseover', () => {
-    gridItem.classList.add('gradient');
+const gridItems = document.querySelectorAll(".grid-item");
+gridItems.forEach((gridItem) => {
+  gridItem.addEventListener("mouseover", () => {
+    gridItem.classList.add("gradient");
   });
-  gridItem.addEventListener('mouseout', () => {
-    gridItem.classList.remove('gradient');
+  gridItem.addEventListener("mouseout", () => {
+    gridItem.classList.remove("gradient");
   });
-}
-);
-
+});
